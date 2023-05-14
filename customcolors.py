f078459ab91aa42015d583c06491cfb54606c548
pluopluo <pluo090807@gmail.com>
2023-05-13 20:14:53 -0700

    testing

---
+++ customcolors.py
@@ -0,0 +1,32 @@
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+cyan = (0,255,255)
+magenta = (255,0,255)
+yellow = (255,255,0)
+white = (255,255,255)
+black = (0,0,0)
+gold = (255,215,0)
+darkgreen = (0,100,0)
+greenyellow = (173,255,47)
+purple = (128,0,128)
+pink = (255,192,203)
+gray = (128,128,128)
+brown = (165,42,42)
+
+
+import random
+def randomcolor():
+    color1 = random.randint(1,255)
+    color2 = random.randint(1,255)
+    color3 = random.randint(1,255)
+
+    color = (color1,color2,color3)
+    return color
+
+
+
+
+
+
